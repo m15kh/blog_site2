92b3e85f5a2d2719a78573140381cdaad8b7d809
liam <m15kh10@email.com>
2023-08-31 19:05:16 +0330

    add delete feature

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("<int:pk>/", PostViewDetail.as_view(), name = "detail_post" ),
     path("new/", PostViewNew.as_view(), name = "new_post" ),
     path("<int:pk>/edit/", PostViewEdit.as_view(), name = "edit_post" ),
+    path("<int:pk>/delete/", PostViewDelete.as_view(), name = "delete_post" ),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView 
+from django.views.generic.edit import CreateView, UpdateView , DeleteView
 from .models import Post
 from django.urls import reverse_lazy
 
@@ -23,3 +23,9 @@ class PostViewEdit(UpdateView):
     model = Post
     template_name = 'blog/edit_post.html'
     fields = ['title', 'content']
+
+
+class PostViewDelete(DeleteView):
+    model = Post
+    template_name = 'blog/delete_post.html'
+    success_url = reverse_lazy('blog:all_post')
