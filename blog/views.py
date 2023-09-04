c06a9e280e93beffe7e4d5a7c232e0a9c2169da8
liam <m15kh10@email.com>
2023-09-04 20:19:46 +0330

    just login users can see post and etc..

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView , DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm
 
-class PostViewList(ListView):
+
+
+
+class PostViewList(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/all_post.html'
+    login_url = 'login'
     
-class PostViewDetail(DetailView):
+class PostViewDetail(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'blog/detail_post.html'
+    login_url = 'login'
 
-class PostViewNew(CreateView):
+
+class PostViewNew(LoginRequiredMixin, CreateView):
     form_class = PostForm
     template_name = 'blog/new_post.html'
     reverse_lazy = 'blog:all_post'
+    login_url = 'login'
 
-class PostViewEdit(UpdateView):
+
+class PostViewEdit(LoginRequiredMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/edit_post.html'
+    login_url = 'login'
 
-class PostViewDelete(DeleteView):
+
+class PostViewDelete(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'blog/delete_post.html'
     success_url = reverse_lazy('blog:all_post')
+    login_url = 'login'
